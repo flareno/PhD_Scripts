eaab24a7b33ffd3d7841209d50f0ee4d49cc0c8a
Federica Lareno Faccini <49164217+flareno@users.noreply.github.com>
2020-11-17 11:13:37 +0100

    Update Extract_Waveform.py

--- Electrophysiology/Spike Sorting/Extract_Waveform.py
+++ Electrophysiology/Spike Sorting/Extract_Waveform.py
@@ -20,20 +20,21 @@ power = '7mW'
 stim = 'NoStim'
 
 #The path of the TDC catalogue file - must be STRING format
-path =fr'D:/F.LARENO.FACCINI/Preliminary Results/Ephy/6336 (Atlas - Male)/h5/{depth}/{protocol}/rbf/No stim/tdc_6336_{depth}_{protocol}_{power}_{stim}_SingleUnit/'
+path =r'\\equipe2-nas1\F.LARENO-FACCINI\BACKUP FEDE\Ephy\Group 15\173 (CM16-Buz - Female)\Experiment\Fixed Delay\P0\tdc_173_EF_P0'
 
 #Name of the experiment, protocol... anything to discriminate the date. Will be used
 #for datasheets/figure labeling - must be STRING format  
-name = f'6336-{depth}-{protocol}-{stim}'
+name = '173-EF-P0'
 
 #Where to save datasheets and figures. If None, nothing will be saved  - must be STRING format
-savedir = r'C:/Users/F.LARENO-FACCINI/Desktop/wf_prova'
+# savedir = r'C:/Users/F.LARENO-FACCINI/Desktop/wf_prova'
+savedir = None
 
 #Specify the channel group to explore as [#]. Feel free to do them all : [0,1,2,3]
 channel_groups=[0]
 
 #If True : close figure automatically (avoids to overhelm screen when looping and debug)
-closefig = True
+closefig = False
 
 #The opacity for the waveform rms. 1. = solid, 0. = transparent 
 wf_alpha =0.2
@@ -75,12 +76,13 @@ for chan_grp in channel_groups:
     clust_list = np.unique(waveforms_label)
     mask=(clust_list >= 0)
     pos_clustlist=clust_list[mask]
-    n_clust=len(pos_clustlist)    
+    n_clust=len(pos_clustlist)   
+    delta = len(clust_list)-len(pos_clustlist)
 
 
     # Figure for waveforms------------------------------------------------------
           
-    for cluster, idx in zip(pos_clustlist, range(n_clust)):
+    for cluster in pos_clustlist:
         fig1 = plt.figure(figsize=(10,12))
         ax1 = fig1.add_subplot(111)
         fig2 = plt.figure()
@@ -96,15 +98,15 @@ for chan_grp in channel_groups:
         for loc, prob_loc in zip(range(len(probe_geometry)), probe_geometry): # N.B. the index of the channels is the real number of the probe site! (the channels at index [0] IS site #0 on the probe)
             x_offset, y_offset = prob_loc[0], prob_loc[1]
             #base_x = np.arange(0,len(waveforms[1,:,loc]),1)  
-            base_x = np.linspace(-15,15,num=len(waveforms[idx+1,:,loc])) #Basic x-array for plot, centered
-            clust_color = 'C{}'.format(idx)
+            base_x = np.linspace(-15,15,num=len(waveforms[cluster+1,:,loc])) #Basic x-array for plot, centered
+            clust_color = 'C{}'.format(cluster)
 
-            wave = waveforms[idx+1,:,loc]+y_offset
+            wave = waveforms[cluster+delta,:,loc]+y_offset
             ax1.plot(base_x+2*x_offset,wave,color=clust_color)
-            ax1.fill_between(base_x+2*x_offset,wave-wf_rms[idx+1],wave+wf_rms[idx+1], color=clust_color,alpha=wf_alpha)
+            ax1.fill_between(base_x+2*x_offset,wave-wf_rms[cluster+delta],wave+wf_rms[cluster+delta], color=clust_color,alpha=wf_alpha)
 
         for i in range(16):
-            ax2.plot(np.arange(0,16,1),waveforms[idx+1].min(axis=0))
+            ax2.plot(np.arange(0,16,1),waveforms[cluster+delta].min(axis=0))
         
         
         if savedir !=None :
@@ -114,11 +116,21 @@ for chan_grp in channel_groups:
                 #File infos 
                 waveform_info = pd.DataFrame(cc.clusters)
                 waveform_info.to_excel(writer, sheet_name='info')
-                for cluster, idx in zip(pos_clustlist, range(n_clust)):
-                    clust_WF = pd.DataFrame(waveforms[idx+1,:,:])      
-                    clust_WF.to_excel(writer,sheet_name='cluster {}'.format(cluster))           
+                for idx in pos_clustlist:
+                    clust_WF = pd.DataFrame(waveforms[idx+delta,:,:])      
+                    clust_WF.to_excel(writer,sheet_name='cluster {}'.format(idx))           
         else : 
             print ('No savedir specified : nothing will be saved')
         
         if closefig==True:
             plt.close('all')
+            
+            
+    if savedir !=None :
+        with pd.ExcelWriter('{}/{}_waveforms_changrp{}.xlsx'.format(savedir,name,chan_grp)) as writer:
+            #File infos 
+            waveform_info = pd.DataFrame(cc.clusters)
+            waveform_info.to_excel(writer, sheet_name='info')
+            for idx in pos_clustlist:
+                clust_WF = pd.DataFrame(waveforms[idx+delta,:,:])      
+                clust_WF.to_excel(writer,sheet_name='cluster {}'.format(idx))           
